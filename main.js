997b60b8fd126520cd44f51df5e89d27f39d6fbb
wrydmoonMCA <juanromero2719@gmail.com>
2024-09-11 15:18:38 -0500

    mca

--- main.js
+++ main.js
@@ -11,3 +11,22 @@ cerrar.addEventListener("click", ()=> {
 
     nav.classList.remove("visible");
 })
+
+function updateBannerImage() {
+    const banner = document.querySelector('.banner');
+    const width = window.innerWidth;
+
+    if (width >= 801) {
+        banner.style.backgroundImage = "url('images/bannermain.jpg')";
+    } else if (width >= 601) {
+        banner.style.backgroundImage = "url('images/prueba.jpg')";
+    } else {
+        banner.style.backgroundImage = "url('images/bannerecmobil.png')";
+    }
+}
+
+// Llama a la función al cargar la página
+updateBannerImage();
+
+// Llama a la función cada vez que la ventana se redimensione
+window.addEventListener('resize', updateBannerImage);
